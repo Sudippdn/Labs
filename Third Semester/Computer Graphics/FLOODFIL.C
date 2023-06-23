4b67bbfaca06d317008132787d7bef591bddb899
Sudip Pradhan <104957400+Sudippdn@users.noreply.github.com>
2023-06-23 23:03:46 +0545

    Add files via upload

---
+++ Third Semester/Computer Graphics/FLOODFIL.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
+void flood (int,int, int,int);
+void main()
+{
+int gd, gm =DETECT;
+clrscr()
+detectgraph(&gd,&gm);
+initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+rectangle(50,50,100,100);
+flood(55,55,12,0);
+getch();
+}
+void flood(int x, int y, int fill_col, int old_col)
+{
+if(getpixel(x,y)==old_col)
+{
+delay(10);
+putpixel(x,y,fill_col);
+flood(x+1,y,fill_col,old_col);
+flood(x-y,y,fill_col, old_col);
+flood(x,y+1,fill_col, old_col);
+flood(x,y-1, fill_col, old_col);
+flood(x+1,y-1, fill_col, old_col);
+flood(x+1,y+1, fill_col, old_col);
+flood(x-1,y-1, fill_col, old_col);
+flood(x-1, y-1, fill_col, old_col);
+}
+}
